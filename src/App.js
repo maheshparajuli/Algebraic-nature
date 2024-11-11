b238bc78a677a7b836b6ba53222dc02fea518251
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-11 18:24:30 +0545

    added pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
- import React from 'react';
- import './App.css';
- import Form from './Components/Form'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Pages/Layout";
+import Home from "./Pages/Home";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
+import NoPage from "./Pages/Nopage";
 
- function App(){
-  return(
-    <div>
-      <Form/>
-    </div>
-
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-
- }
- export default App;
-
+}
