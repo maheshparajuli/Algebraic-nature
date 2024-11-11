9de9add14976c24709496e5d17a24998bce10a70
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-11 22:26:25 +0545

    ok

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -5,15 +5,15 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => {
   return (
     <>
-      <nav className='nav'>
+      <nav>
         <ul className='Header'>
-          <li className='l1'>
+          <li>
             <Link to="/">Home</Link>
           </li>
-          <li className='l2'>
+          <li>
             <Link to="/blogs">Blogs</Link>
           </li>
-          <li className='l3'>
+          <li >
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
