89d22b2b918ca58192e507eb721fcaca5c246cee
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-13 18:09:08 +0545

    learn

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,18 +2,31 @@ import React from 'react';
 import { useState } from 'react';
 
  function Home(){
-    const [name,changeName]=useState("Mahesh");
-    
+   
 
 
-    return(
-        <>
-        <h1 onClick={()=>changeName("biskal")}>{name}</h1>
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: 1964,
+    color: "red"
+  });
 
-    
-        </>
-    );
+ 
+  const updateColor = () => {
+    setCar(soCar => ({ ...soCar, color: "blue" }));
+  };
 
-    
+  return (
+    <div>
+      <h1>{car.brand} {car.model}</h1>
+      <p>Color: {car.color}</p>
+      <button onClick={updateColor}>Change Color</button>
+    </div>
+  );
 }
+
+
+    
+
 export default Home;
