6afef10996454d1ffbcd4f0e4d73bf2b7bf8001f
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-13 17:53:35 +0545

    ...

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 
  function Home(){
+    const [name,changeName]=useState("Mahesh");
+    
+
+
     return(
-        <h1></h1>
+        <>
+        <h1 onClick={()=>changeName("biskal")}>{name}</h1>
+
+    
+        </>
     );
+
     
 }
 export default Home;
