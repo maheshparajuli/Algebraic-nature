913bce4e4972de2b75230a8f75f1237126cfa845
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-14 22:12:53 +0545

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,32 +2,18 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function Home() {
-  const [student, setinfo] = useState({
-    name: "mahesh",
-    class: 7,
-    roll: 124,
-  });
-  const updateis = () => {
-    setinfo((prevstudent) => {
-      return { ...prevstudent, name: "krish" };
-    });
-  };
-
   const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  },[]);
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
 
   return (
     <>
-      <h1>our new student is {student.name}</h1>
-      <button type="button" onClick={updateis}>
-        change name
-      </button>
-      <h2>used {count} times</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
     </>
   );
 }
